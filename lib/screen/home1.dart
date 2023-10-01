ae5ce8dba2e11dd7b6eafcf681b36a908f07a7f5
ashrafarif9 <ashrafarif59@gmail.com>
2023-10-01 23:35:07 +0300

    d

--- lib/screen/home1.dart
+++ lib/screen/home1.dart
@@ -31,63 +31,84 @@ class _home1State extends State<home1> {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.symmetric(vertical: 5),
-        child: Container(
-            height: 130,
-            //   color: Colors.red,
-            child: ListView.builder(
-              itemBuilder: (context, index) {
-                return Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 2),
-                  child: Column(
-                    children: [
-                      Container(
-                        //height:80,
-                        padding: EdgeInsets.all(3),
-                        decoration: BoxDecoration(
-                          gradient: LinearGradient(
-                            colors: index == 1
-                                ? [Colors.purple, Colors.brown]
-                                : [Colors.red, Colors.orange],
-                            begin: Alignment.topLeft,
-                            end: Alignment.bottomRight,
-                          ),
-                          color: Colors.red,
-                          shape: BoxShape.circle,
-                        ),
-                        child: CircleAvatar(
-                          radius: 37,
-                          backgroundColor: Colors.white,
-                          child: CircleAvatar(
-                            radius: 35,
-                            backgroundImage: AssetImage(profile[index]),
-                            child:index == 1
-                                ? Container(
-                              margin: EdgeInsets.symmetric(vertical: 30),
-                              width: 30,
-                              height: 20,
-                              decoration : BoxDecoration(
-                                  color: Colors.purpleAccent,
+        child: Column(
+          children: [
+            Container(
+                height: 100,
+                //   color: Colors.red,
+                child: ListView.builder(
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 5),
+                      child: Column(
+                        children: [
+                          Container(
+                            //height:80,
+                            padding: EdgeInsets.all(3),
+                            decoration: BoxDecoration(
+                              gradient: LinearGradient(
+                                colors: index == 1
+                                    ? [Colors.purple, Colors.brown]
+                                    : [Colors.red, Colors.orange],
+                                begin: Alignment.topLeft,
+                                end: Alignment.bottomRight,
+                              ),
+                              color: Colors.red,
+                              shape: BoxShape.circle,
+                            ),
+                            child: CircleAvatar(
+                              radius: 37,
+                              backgroundColor: Colors.white,
+                              child: CircleAvatar(
+                                radius: 35,
+                                backgroundImage: AssetImage(profile[index]),
+                                child:index == 1
+                                    ? Container(
+                                  margin: EdgeInsets.symmetric(vertical: 30),
+                                  width: 30,
+                                  height: 20,
+                                  decoration : BoxDecoration(
+                                      color: Colors.purpleAccent,
+
+                                  ),
+                                  child: Center(
+                                    child: Text('Live', style: TextStyle(
+                                      fontSize: 10
+                                    ),),
+                                  )
+                                )
+                                      : null,
 
                               ),
-                              child: Center(
-                                child: Text('Live', style: TextStyle(
-                                  fontSize: 10
-                                ),),
-                              )
-                            )
-                                  : null,
-
+                            ),
                           ),
-                        ),
+                          Center(child: Text(name[index]))
+                        ],
                       ),
-                      Center(child: Text(name[index]))
-                    ],
-                  ),
-                );
-              },
-              itemCount: profile.length,
-              scrollDirection: Axis.horizontal,
-            )),
+                    );
+                  },
+                  itemCount: profile.length,
+                  scrollDirection: Axis.horizontal,
+                )),
+            Divider(color: Colors.grey.shade400,thickness: 1,),
+            SizedBox(height: 1.h,),
+            ListTile(
+              leading: CircleAvatar(
+                //radius: 35,
+                backgroundImage: AssetImage('image/profile4.jpg'),
+
+              ),
+              trailing: Icon(Icons.more_horiz,color: Colors.black,),
+              title: Row(children: [
+                Text('joshua_l',style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),),
+                SizedBox(width: 2,),
+                Icon(Icons.check_circle,color: Colors.blueAccent,size: 15,)
+              ],),
+              subtitle: Text('Tokyo, Japan'),
+            ),
+            Image(image: AssetImage('pciture1.PNG'))
+          ],
+        ),
       ),
       // body: Container(
       //   child: ListView.builder(
